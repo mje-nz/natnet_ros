67fb65a55e0e81d5f7b187188bf30e67e0a2b187
Matthew Edwards <mje-nz@users.noreply.github.com>
2017-11-24 20:08:14 +1300

    Add coverage to tests

--- ci_tests.sh
+++ ci_tests.sh
@@ -7,4 +7,5 @@ travis_run catkin build --no-status --summarize
 travis_run source install/setup.bash
 travis_run catkin build --catkin-make-args run_tests -- --no-status --summarize
 travis_run catkin_test_results --verbose
-
+travis_run cp ~/.ros/.coverage out/
+travis_run cd out && python -m coverage report && python -m coverage xml -i
