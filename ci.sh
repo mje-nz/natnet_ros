b9f6b6f20af338357bd439651bd8e0f16488bbac
Matthew Edwards <mje-nz@users.noreply.github.com>
2019-09-05 21:17:11 +1200

    Fix codecov

--- ci.sh
+++ ci.sh
@@ -8,17 +8,16 @@ travis_run apt-get update
 travis_run apt-get install -y python-catkin-tools python-coverage python-pip
 
 travis_run rosdep install -y --from-paths src --ignore-src
-travis_run catkin config --install
+travis_run catkin config
 travis_run catkin build --no-status --summarize
-source install/setup.bash
+source devel/setup.bash
 
 travis_run catkin build --catkin-make-args run_tests -- --no-status --summarize
 travis_run catkin_test_results --verbose
 
 mkdir out
-cp ~/.ros/.coverage out/
-cp src/natnet_ros/.codecov.yml out/
-cd out
+cp ~/.ros/.coverage .
 travis_run python -m coverage report
 travis_run python -m coverage xml -i
-travis_run bash <(curl -s https://codecov.io/bash)
+# https://github.com/codecov/codecov-bash/issues/100
+travis_run bash <(curl -s https://codecov.io/bash) -y src/natnet_ros/.codecov.yml
